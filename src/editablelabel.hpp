d46724091f0da4168d75614c6972281a0661bcaa
Séverin Lemaignan <severin@guakamole.org>
2015-10-15 20:29:08 +0100

    Added missing 'editablelabel' class

---
+++ src/editablelabel.hpp
@@ -0,0 +1,33 @@
+#ifndef EDITABLELABEL_HPP
+#define EDITABLELABEL_HPP
+
+#include <QGraphicsTextItem>
+#include <QKeyEvent>
+
+class EditableLabel : public QGraphicsTextItem {
+  Q_OBJECT
+ public:
+  EditableLabel(QGraphicsItem *parent) : QGraphicsTextItem(parent) {
+    this->setDefaultTextColor(Qt::white);
+    this->setTextInteractionFlags(Qt::TextEditorInteraction);
+    this->setCursor(Qt::IBeamCursor);
+  }
+
+  void keyPressEvent(QKeyEvent *event) override {
+    if ((event->key() == Qt::Key_Enter) || (event->key() == Qt::Key_Return)) {
+      clearFocus();
+    } else {
+      QGraphicsTextItem::keyPressEvent(event);
+    }
+  }
+
+  void focusOutEvent(QFocusEvent *event) override {
+    emit contentUpdated(toPlainText());
+    QGraphicsTextItem::focusOutEvent(event);
+  }
+
+signals:
+  void contentUpdated(QString content);
+};
+
+#endif
