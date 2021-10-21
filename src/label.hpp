756f4ddac685c2503e8effa66f1a7d0fc3050444
Séverin Lemaignan <severin.lemaignan@pal-robotics.com>
2021-10-21 15:55:51 +0200

    use SPRING partner names instead of cognitive functions

--- src/label.hpp
+++ src/label.hpp
@@ -4,44 +4,46 @@
 #include <map>
 #include <string>
 
+// the order of values in this enum determine the order of icons in
+// the UI + the order by which the nodes are listed in the markdown output
 enum class Label {
-    PERCEPTION,
-    ATTENTION,
-    LANGUAGE,
-    DECISION_MAKING,
-    MEMORY,
-    REASONING,
-    SOCIOCOGNITION,
-    EMOTIONS,
-    LEARNING,
-    ACTION,
+    REASONING,        // CVUT
+    MEMORY,           // UNITN
+    PERCEPTION,       // INRIA
+    LANGUAGE,         // BIU
+    DECISION_MAKING,  // HWU
+    SOCIOCOGNITION,   // ERM
+    ATTENTION,        // PAL
+    //    EMOTIONS,
+    //    LEARNING,
+    //    ACTION,
     OTHER
 };
 
 static const std::map<Label, std::string> LABEL_NAMES{
-    {Label::PERCEPTION, "Perception"},
-    {Label::ATTENTION, "Attention"},
-    {Label::LANGUAGE, "Language"},
-    {Label::DECISION_MAKING, "Decision making"},
-    {Label::MEMORY, "Memory"},
-    {Label::REASONING, "Reasoning"},
-    {Label::SOCIOCOGNITION, "Socio-cog."},
-    {Label::EMOTIONS, "Emotions"},
-    {Label::LEARNING, "Learning"},
-    {Label::ACTION, "Action"},
+    {Label::PERCEPTION, "INRIA"},
+    {Label::ATTENTION, "PAL"},
+    {Label::LANGUAGE, "BIU"},
+    {Label::DECISION_MAKING, "HWU"},
+    {Label::MEMORY, "UNITN"},
+    {Label::REASONING, "CVUT"},
+    {Label::SOCIOCOGNITION, "ERM"},
+    //    {Label::EMOTIONS, "Emotions"},
+    //    {Label::LEARNING, "Learning"},
+    //    {Label::ACTION, "Action"},
     {Label::OTHER, "Other"}};
 
 static const std::map<Label, std::string> LABEL_COLORS{
-    {Label::PERCEPTION, "#53399d"},
-    {Label::ATTENTION, "#0476aa"},
-    {Label::LANGUAGE, "#9d398b"},
-    {Label::MEMORY, "#ffaa7f"},
-    {Label::DECISION_MAKING, "#e1d620"},
-    {Label::REASONING, "#c28e25"},
-    {Label::SOCIOCOGNITION, "#399d40"},
-    {Label::EMOTIONS, "#a11012"},
-    {Label::LEARNING, "#2e13ff"},
-    {Label::ACTION, "#75bdff"},
+    {Label::ATTENTION, "#53399d"},
+    {Label::PERCEPTION, "#0476aa"},
+    {Label::REASONING, "#9d398b"},
+    {Label::DECISION_MAKING, "#ffaa7f"},
+    {Label::SOCIOCOGNITION, "#e1d620"},
+    {Label::LANGUAGE, "#c28e25"},
+    {Label::MEMORY, "#399d40"},
+    //    {Label::EMOTIONS, "#a11012"},
+    //    {Label::LEARNING, "#2e13ff"},
+    //    {Label::ACTION, "#75bdff"},
     {Label::OTHER, "#1a1a1a"}};
 
 Label get_label_by_name(const std::string& name);
