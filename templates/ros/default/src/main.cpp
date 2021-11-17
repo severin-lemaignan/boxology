4ecf33c579dda24af201b97316e9433bca5aae60
Séverin Lemaignan <severin.lemaignan@pal-robotics.com>
2021-11-17 20:21:49 +0100

    [ros] add license to main.cpp + fix invalid C++ code

--- templates/ros/default/src/main.cpp
+++ templates/ros/default/src/main.cpp
@@ -1,4 +1,25 @@
-#include <sstream>
+/*
+ Copyright 2021-2025, SPRING Consortium
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+*/
+
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
@@ -7,7 +28,7 @@
 {% endfor %}
 
 {% for sub in inputs %}
-{% if not sub.type == "tf" %}
+{% if not (sub.type == "tf") %}
 void {{ sub.short }}Callback(const {{ sub.datatype.0 }}::{{ sub.datatype.1 }}::ConstPtr& msg)
 {
   ROS_INFO_STREAM("{{ id }}: received message: " << msg);
@@ -38,25 +59,8 @@ int main(int argc, char **argv) {
     {% endif %}
     {% endfor %}
 
-    ros::Rate loop_rate(10);
 
-    int count = 0;
-    while (ros::ok()) {
-        std_msgs::String msg;
-
-        std::stringstream ss;
-        ss << "hello world " << count;
-        msg.data = ss.str();
-
-        ROS_INFO("%s", msg.data.c_str());
-
-        chatter_pub.publish(msg);
-
-        ros::spinOnce();
-
-        loop_rate.sleep();
-        ++count;
-    }
-
+    ROS_INFO("Node {{ id }} launched and ready.");
+    ros::spin();
     return 0;
 }
