b40ae3654dda6eaa4f18e5dff28f0d2571c74b0a
Séverin Lemaignan <severin.lemaignan@pal-robotics.com>
2021-11-17 07:56:13 +0100

    [ros] fetch_released_nodes.sh: pull instead of clone if repo already exists

--- templates/ros/fetch_released_nodes.sh
+++ templates/ros/fetch_released_nodes.sh
@@ -5,7 +5,14 @@
 
 {% for node in nodes %}
 {% if not node.generate %}
-echo "Cloning {{ node.name }} (ROS node: {{ node.id }}), branch: {{ node.version }} to src/ ..."
-git clone --depth 1 --branch {{ node.version }} {{ node.repo }} src/{{ node.id }}
+if cd src/{{ node.id }}; 
+then
+    echo "{{ node.id }}: pulling latest changes from {{ node.version }}...";
+    git pull origin {{ node.version }}; 
+else 
+
+    echo "Cloning {{ node.name }} (ROS node: {{ node.id }}), branch: {{ node.version }} to src/ ..."
+    git clone --depth 1 --branch {{ node.version }} {{ node.repo }} src/{{ node.id }};
+fi
 {% endif %}
 {% endfor %}
