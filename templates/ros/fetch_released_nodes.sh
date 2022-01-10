bb02a47e6f289329bad1054498dd5762be4edaab
Séverin Lemaignan <severin.lemaignan@pal-robotics.com>
2022-01-10 13:59:27 +0100

    add support for fetching nodes from repo subfolders

--- templates/ros/fetch_released_nodes.sh
+++ templates/ros/fetch_released_nodes.sh
@@ -5,14 +5,22 @@
 
 {% for node in nodes %}
 {% if not node.generate %}
-if cd src/{{ node.id }}; 
-then
-    echo "{{ node.id }}: pulling latest changes from {{ node.version }}...";
-    git pull origin {{ node.version }}; 
-else 
 
-    echo "Cloning {{ node.name }} (ROS node: {{ node.id }}), branch: {{ node.version }} to src/ ..."
-    git clone --depth 1 --branch {{ node.version }} {{ node.repo }} src/{{ node.id }};
+NODE_PATH=src/{{ node.id }}
+if [ -d "$NODE_PATH" ]; then
+    rm -rf $NODE_PATH;
 fi
+cd $NODE_PATH
+{% if existsIn(node, "repo_subfolder") %}
+echo "Cloning {{ node.name }} (ROS node: {{ node.id }}), branch: {{ node.version }} to /tmp ..."
+rm -rf /tmp/{{ node.id }}
+git clone --depth 1 --branch {{ node.version }} {{ node.repo }} /tmp/{{ node.id }}
+echo "Copying subfolder {{ node.repo_subfolder }} to src/ ..."
+cp -R /tmp/{{ node.id }}/{{ node.repo_subfolder }} src/{{ node.id }}
+{% else %}
+echo "Cloning {{ node.name }} (ROS node: {{ node.id }}), branch: {{ node.version }} to src/ ..."
+git clone --depth 1 --branch {{ node.version }} {{ node.repo }} src/{{ node.id }}
+{% endif %}
 {% endif %}
 {% endfor %}
+
